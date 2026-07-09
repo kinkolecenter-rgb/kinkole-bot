a7bfc9b73fc0d8c075c45ad0bf5666cc48efafeb
kinkolecenter-rgb <kinkolecenter@winner.bet>
2026-07-09 14:40:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: process.env.PORT || 3000,
   monNumero: process.env.MON_NUMERO || '243904246049',
+  monLid: process.env.MON_LID || '204685424214253',
   redis: {
       host: process.env.UPSTASH_REDIS_HOST,
       port: process.env.UPSTASH_REDIS_PORT || 6379,
