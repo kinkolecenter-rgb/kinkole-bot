c5dbba5574186be225e70a4cdf10dd5454d6b1e4
kinkolecenter-rgb <kinkolecenter@winner.bet>
2026-07-11 10:41:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -33,11 +33,14 @@ module.exports = {
     },
 
     managers: {
-    '243900014909@s.whatsapp.net': { nom: 'Eric',     role: 'Manager' },
-    '243903533322@s.whatsapp.net': { nom: 'Timothée', role: 'Manager' },
-    '243898170279@s.whatsapp.net': { nom: 'Déborah',  role: 'Ass. Manager' },
-    '243904323374@s.whatsapp.net': { nom: 'Trésor',   role: 'Ass. Manager' }
-    },
+    '178499008630811@lid': { nom: 'Collaborateur 1', role: 'Manager' },
+    '90263603159168@lid':  { nom: 'Collaborateur 2', role: 'Manager' },
+    '42967356150013@lid':  { nom: 'Collaborateur 3', role: 'Ass. Manager' },
+    '265029714768018@lid': { nom: 'Collaborateur 4', role: 'Ass. Manager' },
+    '152059408036054@lid': { nom: 'Collaborateur 5', role: 'Manager' },
+    '169230989307948@lid': { nom: 'Collaborateur 6', role: 'Manager' },
+    '265515029283001@lid': { nom: 'Collaborateur 7', role: 'Ass. Manager' }
+},
     heuresBrief: ['10:00', '12:00', '17:00'],
     collaborateurs: ['Eric', 'Timothée', 'Déborah', 'Trésor']
 };
